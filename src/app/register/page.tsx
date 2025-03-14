de3703053b1c1ed3e46559e1e8abefffb449b10b
Hikaru <hikaru.uchita@gmail.com>
2025-03-14 20:02:42 +0800

    Re-add correct register folder

---
+++ src/app/register/page.tsx
@@ -0,0 +1,20 @@
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+import { RegisterForm } from "@/app/register/register-form";
+
+export default async function Page() {
+  const session = await getServerSession();
+
+  if (session) {
+    redirect("/");
+  }
+
+  return (
+    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+      <div className="w-full max-w-sm">
+        git
+        <RegisterForm />
+      </div>
+    </div>
+  );
+}
